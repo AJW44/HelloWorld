0ceb1b7181f9042f5f7a62eea8eda224e5a8d213
AJW44 <bookkeepingajw@gmail.com>
2018-07-06 20:35:41 +1200

    Change of comment with new branch

--- MyFirstProject/src/HelloWorldExample.java
+++ MyFirstProject/src/HelloWorldExample.java
@@ -2,7 +2,7 @@
 public class HelloWorldExample {
 
 	public static void main(String[] args) {
-		// Output Hello World to the user
+		// Change of comment with new branch
 		System.out.println("Hello World");
 	}
 
